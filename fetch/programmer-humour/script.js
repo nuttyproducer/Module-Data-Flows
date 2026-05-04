9d360089323c7be03068d26137679ff630a6907f
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-04 15:21:10 +0200

    Implement XKCD Comics feature with HTML structure and JavaScript functionality

---
+++ fetch/programmer-humour/script.js
@@ -0,0 +1,25 @@
+const comicImage = document.getElementById("comic-image");
+const comicAlt = document.getElementById("comic-alt");
+
+function fetchComic() {
+  fetch("https://xkcd.now.sh/?comic=latest")
+    .then((response) => {
+      if (!response.ok) {
+        // Error handeling
+        throw new Error("Network fetch was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      comicImage.src = data.img;
+      comicImage.alt = data.alt;
+      comicAlt.textContent = data.alt;
+    })
+    .catch((error) => {
+      // Error handeling
+      console.error("Fetch error:", error);
+    });
+}
+
+fetchComic();
