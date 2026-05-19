040946734289e09a26deeb9b5e1a044425740e47
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-19 23:30:48 +0200

    Refactor cowsay implementation to include default message and ASCII art

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -1,30 +1,46 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
 
 // 1. Accept arguments
 
-// how will you accept arguments?
+const cowInput = process.argv[2]; // first argument after the script name
 
 // 2. Make supplies for our speech bubble
 
-let topLine = '_';
-let bottomLine = '-';
-let saying = '';
+let topLine = "_";
+let bottomLine = "-";
+let saying = "";
 
 // 3. Make a cow that takes a string
 
 function cowsay(saying) {
-// how will you make the speech bubble contain the text?
+  // default message when nothing is provided
+  if (!saying) {
+    saying = "Moo!";
+  }
 
-// where will the cow picture go?
+  // build bubble lengths: message + one space both sides
+  const bubbleLength = saying.length + 2;
 
-// how will you account for the parameter being empty?
+  const top = " " + topLine.repeat(bubbleLength);
+  const bottom = " " + bottomLine.repeat(bubbleLength);
+  const middle = `< ${saying} >`;
 
+  // cow ASCII art
+  const cowArt =
+    "        \\\   ^__^\n" +
+    "         \\\  (oo)\\_______\n" +
+    "            (__)\\       )\\/\\\n" +
+    "                ||----w |\n" +
+    "                ||     ||";
+
+  // combine parts and return full string
+  return `${top}\n${middle}\n${bottom}\n${cowArt}`;
 }
 
 //4. Pipe argument into cowsay function and return a cow
 
-// how will you log this to the console?
+console.log(cowsay(cowInput));
--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -1,18 +1,44 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
 
-// 1. Make  a command line interface.
+// 1. Make a command line interface using readline
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // 2. Make supplies for our speech bubble
+const topLine = "_";
+const bottomLine = "-";
 
 // 3. Make a cow that takes a string
-
 const cow = (saying) => {
-    // how did you make the cow before?
-}
+  if (!saying) {
+    saying = "Moo!";
+  }
 
-// 4. Use readline to get a string from the terminal 
-// (with a prompt so it's clearer what we want)
+  const bubbleLength = saying.length + 2;
+  const top = " " + topLine.repeat(bubbleLength);
+  const bottom = " " + bottomLine.repeat(bubbleLength);
+  const middle = `< ${saying} >`;
+
+  const cowArt = // Added some cow dung in the grassy fields... :)
+    "        \\   ^__^                        °\n" +
+    "         \\  (oo)\\_______                 °\n" +
+    "            (__)\\       )\\/\\            °_\n" +
+    "                ||----w |               (_)\n" +
+    "                ||     ||              (___)";
+
+  return `${top}\n${middle}\n${bottom}\n${cowArt}`;
+};
+
+// 4. Use readline to get a string from the terminal
+rl.question("What should the cow say? ", (answer) => {
+  console.log(cow(answer));
+  rl.close();
+});
