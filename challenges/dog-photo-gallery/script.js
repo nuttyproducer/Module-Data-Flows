3bbc6a582a6f6d0abfc201d1a0b0dd33b4bb6d61
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-20 22:47:07 +0200

    Add JavaScript setup for Dog Photo Gallery feature

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,7 @@
+// API URL
+const url = "https://dog.ceo/api/breeds/image/random";
+
+// Get the elements from the HTML
+
+const addButton = document.getElementById("add-btn");
+const resetButton = document.getElementById("clear-btn");
