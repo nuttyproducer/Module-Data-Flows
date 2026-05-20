af37ab31179d76ab76ce88408c1e94b6d2acce98
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-20 22:54:35 +0200

    Add function to fetch and log random dog images

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -5,3 +5,18 @@ const url = "https://dog.ceo/api/breeds/image/random";
 
 const addButton = document.getElementById("add-btn");
 const resetButton = document.getElementById("clear-btn");
+const dogGallery = document.getElementById("dog-gallery");
+
+// Write a function that fetches a dog image and appends it to the gallery
+
+function addDogsImages() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+// Add a click listener to the add button
+
+// Add a click listener to the clear button
