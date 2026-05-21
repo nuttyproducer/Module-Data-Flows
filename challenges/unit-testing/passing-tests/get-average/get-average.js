72a9cab20ebce17c1529466d0751b2635c8316d6
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-21 16:48:19 +0200

    Implement average function to calculate the average of numbers in an array, excluding strings

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,18 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let sum = 0;
+  let count = 0;
+
+  for (const item of numbers) {
+    if (typeof item === "number") {
+      sum = sum + item;
+      count = count + 1;
+    }
+  }
+
+  return sum / count;
+}
 
 module.exports = average;
