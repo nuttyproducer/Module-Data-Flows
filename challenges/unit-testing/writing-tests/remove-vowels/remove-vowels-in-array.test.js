59f859b44d551a7e9f5a46bf5d1258903714126e
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-21 15:59:18 +0200

    Improve test descriptions and add clarity to removeVowelsFromWords tests

--- challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
+++ challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
@@ -1,11 +1,23 @@
 let removeVowelsFromWords = require("./remove-vowels-in-array");
 
-test("remove vowels from all words in array", function () {
+test("removes vowels from all words in the array", function () {
   // Arrange
+  const words = ["Irina", "Etza", "Daniel"];
+
   // Act
+  const result = removeVowelsFromWords(words);
+
   // Assert
+  expect(result).toEqual(["rn", "tz", "Dnl"]);
 });
 
-// example
-// input: ["Irina", "Etza", "Daniel"]
-// expected output: ["rn", "tz", "Dnl"]
+test("the original array is not changed", function () {
+  // Arrange
+  const words = ["Irina", "Etza", "Daniel"];
+
+  // Act
+  removeVowelsFromWords(words);
+
+  // Assert
+  expect(words).toEqual(["Irina", "Etza", "Daniel"]);
+});
