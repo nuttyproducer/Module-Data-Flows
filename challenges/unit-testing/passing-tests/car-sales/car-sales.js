3f94eecfbd04179c404df36a60400d44d0008ba7
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-21 16:42:17 +0200

    Implement sales function to calculate total sales by car make

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,17 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  const totals = {};
+
+  for (const car of carsSold) {
+    if (totals[car.make] !== undefined) {
+      // brand already exists
+      totals[car.make] = totals[car.make] + car.price;
+    } else {
+      // brand seen for first time
+      totals[car.make] = car.price;
+    }
+  }
+
+  return totals;
+}
 
 module.exports = sales;
