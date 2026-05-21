3f6d02d8c6c0dd3a9b0ce5ba7d049dffd5a3ed84
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-21 16:02:37 +0200

    Fix case sensitivity in vowel removal logic in removeVowels function

--- challenges/unit-testing/writing-tests/remove-vowels/remove-vowels.js
+++ challenges/unit-testing/writing-tests/remove-vowels/remove-vowels.js
@@ -5,11 +5,11 @@ function removeVowels(word) {
 
   characters.forEach(function (character) {
     if (
-      character !== "a" &&
-      character !== "o" &&
-      character !== "i" &&
-      character !== "e" &&
-      character !== "u"
+      character.toLowerCase() !== "a" &&
+      character.toLowerCase() !== "o" &&
+      character.toLowerCase() !== "i" &&
+      character.toLowerCase() !== "e" &&
+      character.toLowerCase() !== "u"
     ) {
       result.push(character);
     }
@@ -26,4 +26,7 @@ module.exports = removeVowels;
   let result = removeVowels('samuel');
 
   what is the value of result?
+
+  Result: sml
+
 */
