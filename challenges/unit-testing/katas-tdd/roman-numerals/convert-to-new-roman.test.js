58ef2241a70db742e576c56cdbf9e70a2560ab04
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-22 12:51:37 +0200

    Refactor tests for convertToNewRoman to use arrow functions and improve readability

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -1,7 +1,33 @@
 let convertToNewRoman = require("./convert-to-new-roman");
 
-test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+test("returns I if passed 1", () => {
+  expect(convertToNewRoman(1)).toEqual("I");
+});
+
+test("returns IV for 4", () => {
+  expect(convertToNewRoman(4)).toEqual("IV");
+});
+
+test("returns IX for 9", () => {
+  expect(convertToNewRoman(9)).toEqual("IX");
+});
+
+test("returns XIV for 14", () => {
+  expect(convertToNewRoman(14)).toEqual("XIV");
+});
+
+test("returns XLIV for 44", () => {
+  expect(convertToNewRoman(44)).toEqual("XLIV");
+});
+
+test("returns XCIX for 99", () => {
+  expect(convertToNewRoman(99)).toEqual("XCIX");
+});
+
+test("returns CD for 400", () => {
+  expect(convertToNewRoman(400)).toEqual("CD");
+});
+
+test("returns CMXLIV for 944", () => {
+  expect(convertToNewRoman(944)).toEqual("CMXLIV");
 });
