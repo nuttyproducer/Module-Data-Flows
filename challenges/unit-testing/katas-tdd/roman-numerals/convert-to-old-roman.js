d405d43c51d817f42032d9a54e25a7490f1f2992
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-22 12:52:52 +0200

    Implement convertToOldRoman function to convert integers to old Roman numerals

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,17 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+  const values = [1000, 500, 100, 50, 10, 5, 1];
+  const symbols = ["M", "D", "C", "L", "X", "V", "I"];
+
+  let result = "";
+
+  for (let i = 0; i < values.length; i++) {
+    while (n >= values[i]) {
+      result += symbols[i];
+      n -= values[i];
+    }
+  }
+
+  return result;
+}
 
 module.exports = convertToOldRoman;
