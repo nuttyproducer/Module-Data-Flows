52a026a965da1c424180b4a04ee6c77f827c7412
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-05-22 12:51:50 +0200

    Implement convertToNewRoman function to convert integers to Roman numerals

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,33 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+  // list of values and their roman symbols, largest first
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const symbols = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+
+  let result = "";
+
+  for (let i = 0; i < values.length; i++) {
+    // keep subtracting the value while n is big enough
+    while (n >= values[i]) {
+      result += symbols[i];
+      n -= values[i];
+    }
+  }
+
+  return result;
+}
 
 module.exports = convertToNewRoman;
